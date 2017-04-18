1bbdb373f74d896e354baac38f60e788afdf4bc1
celsowhite <celso.white13@gmail.com>
2017-04-18 10:50:48 -0400

    Update scss scaffold and add js utility functions.

---
+++ js/utilities.js
@@ -0,0 +1,29 @@
+/*================================= 
+DEBOUCE
+=================================*/
+
+// Handle taxing js tasks
+// https://davidwalsh.name/javascript-debounce-function
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+/*================================= 
+MEDIA QUERIES
+=================================*/
+
+// Interactions just for desktop
+
+const mq = window.matchMedia( "(min-width: 1024px)" );
